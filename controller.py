8f2b95413c8ec497228e3959768d4051f1d442c3
Kakimzhanova Kyz-Zhibek <z.kakimzhanova@mail.ru>
2019-09-12 18:10:15 +0600

    jumping dino

--- controller.py
+++ controller.py
@@ -1,12 +1,21 @@
 from pynput.keyboard import Key, Controller as KeyController
+from pynput.mouse import Button, Controller as MouseController
 import time
 
 class Controller:
     def __init__(self):
         self.keyboard = KeyController()
+        self.mouse = MouseController()
 
     def press_space(self):
         self.keyboard.press(Key.space)
         time.sleep(0.05)
         print("jump")
         self.keyboard.release(Key.space)
+
+    def click_left(self):
+        print("click")
+        self.mouse.click(Button.left)
+
+    def set_mouse_position(self, x, y):
+        self.mouse.position = (int(x), int(y))
--- run.py
+++ run.py
@@ -1,13 +1,19 @@
 import cv2
 import numpy as np
+import webbrowser
+import time
 
-#from vision import Vision
 from controller import Controller
-#from game import Game
 
-#vision = Vision()
+url = 'index.html'
+webbrowser.open(url, new=2)  # open in new tab
+
+time.sleep(5)
 controller = Controller()
-controller.press_space()
-#game = Game(vision, controller)
+controller.set_mouse_position(250, 250)
+controller.click_left()
+time.sleep(5)
+while True:
+    controller.press_space()
+    time.sleep(2)
 
-#game.run()
