6ca7eb90997e456766ebf7650232b17b3547611d
Kakimzhanova Kyz-Zhibek <z.kakimzhanova@mail.ru>
2019-09-14 01:43:03 +0600

    add simple analisys

--- run.py
+++ run.py
@@ -4,16 +4,25 @@ import webbrowser
 import time
 
 from controller import Controller
+from vision import Vision
 
-url = 'index.html'
+url = 'https://chromedino.com/'
 webbrowser.open(url, new=2)  # open in new tab
 
 time.sleep(5)
 controller = Controller()
+vision = Vision()
+
 controller.set_mouse_position(250, 250)
-controller.click_left()
-time.sleep(5)
+controller.click_left() #click to set active window with game
+controller.press_space() #start game
+controller.press_space() #start game
+vision.find_cactus()
+
 while True:
-    controller.press_space()
-    time.sleep(2)
+    if (vision.find_cactus()):
+        controller.press_space()
+        time.sleep(0.1)
+    time.sleep(0.0001)
+    #time.sleep(0.1)
 
--- vision.py
+++ vision.py
@@ -2,17 +2,47 @@ import cv2
 from mss import mss
 from PIL import Image
 from matplotlib import pyplot as plt
+import numpy as np
 
 class Vision:
     def __init__(self):
         self.monitor = {'top': 0, 'left': 0, 'width': 1366, 'height': 768}#screen size
         self.screen = mss()
+        self.dino_template = cv2.imread('assets/dino.png', 0)
 
     def take_screenshot(self):
         img = self.screen.grab(self.monitor)
+        img = Image.frombytes('RGB', img.size, img.rgb)
         return img
 
-vision = Vision()
-img = vision.take_screenshot()
-imgplot = plt.imshow(img)
-plt.show()
+    def find_template(self, screenshot, template):
+        screenshot_array = np.array(screenshot)
+        screenshot_array = cv2.cvtColor(screenshot_array, cv2.COLOR_RGB2GRAY)
+        res = cv2.matchTemplate(screenshot_array, template, cv2.TM_CCOEFF_NORMED)
+        min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)
+        return max_loc
+
+    #grab small area next to dino where trees can apear
+    def take_small_screenshot(self, dino):  
+        box = {'top': dino[1] + 10, 'left': dino[0] + 20, 'width': 40, 'height': 50}
+        img = self.screen.grab(box)
+        return img
+
+    def find_cactus(self):
+        screenshot = self.take_screenshot()
+        screenshot_copy = screenshot.copy()
+        dino = self.find_template(screenshot_copy, self.dino_template)
+        print(dino)
+        box = (dino[0] + 43, dino[1] - 20, dino[0] + 160, dino[1] + 50)
+        #box = (0, 0, 20, 50)
+        print(box)
+        finding_area = screenshot.crop(box)
+        #print(finding_area.size)
+        #finding_area.show()
+        #finding_area_array = np.array(finding_area)
+        #plt.imshow(finding_area_array,cmap = 'gray')
+        #plt.show()
+        a = np.array(finding_area)
+        sum = a.sum()
+        print(sum)
+        return(sum < 5900000)
